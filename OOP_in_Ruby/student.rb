ff4d1e8531036d1e6c82dccd8ea30ff346163397
temawm <temafroloff@gmail.com>
2024-10-19 11:45:03 +0300

    lab2_task8 add_validates_for_git_telegram_and_email

--- OOP_in_Ruby/student.rb
+++ OOP_in_Ruby/student.rb
@@ -8,13 +8,25 @@ class Student
     @first_name = first_name
     @middle_name = middle_name
     self.phone = phone
-    @telegram = telegram
-    @email = email
-    @github = github
+    self.telegram = telegram
+    self.email = email
+    self.github = github
   end
 
   def self.valid_phone_number?(phone)
     phone =~ /^\d{10,15}$/
+  end
+
+  def self.valid_telegram?(telegram)
+    telegram.start_with?("@")
+  end
+
+  def self.valid_github?(github)
+    github.start_with?("https://github.com/")
+  end
+
+  def self.valid_email?(email)
+    email.include?("@")
   end
 
   def phone
@@ -26,6 +38,30 @@ class Student
       @phone = phone
     else
       raise ArgumentError, "Некорректный номер телефона"
+    end
+  end
+
+  def telegram=(telegram)
+    if telegram.nil? || Student.valid_telegram?(telegram)
+      @telegram = telegram
+    else
+      raise ArgumentError, "Некорректный Telegram"
+    end
+  end
+
+  def email=(email)
+    if email.nil? || Student.valid_email?(email)
+      @email = email
+    else
+      raise ArgumentError, "Некорректная почта"
+    end
+  end
+
+  def github=(github)
+    if github.nil? || Student.valid_github?(github)
+      @github = github
+    else
+      raise ArgumentError, "Некорректная ссылка на GitHub"
     end
   end
 
