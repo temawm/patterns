fc5c8c2171a3812b15d3a3444a45242f33a50f73
temawm <temafroloff@gmail.com>
2024-10-19 11:40:25 +0300

    lab2_task7 create_validate_number_method

--- OOP_in_Ruby/student.rb
+++ OOP_in_Ruby/student.rb
@@ -1,17 +1,34 @@
 class Student
   attr_reader :id, :last_name, :first_name, :middle_name
-  attr_accessor :phone, :telegram, :email, :github
+  attr_accessor :telegram, :email, :github
 
   def initialize(id:, last_name:, first_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
     @last_name = last_name
     @first_name = first_name
     @middle_name = middle_name
-    @phone = phone
+    self.phone = phone
     @telegram = telegram
     @email = email
     @github = github
   end
+
+  def self.valid_phone_number?(phone)
+    phone =~ /^\d{10,15}$/
+  end
+
+  def phone
+    @phone
+  end
+
+  def phone=(phone)
+    if phone.nil? || Student.valid_phone_number?(phone)
+      @phone = phone
+    else
+      raise ArgumentError, "Некорректный номер телефона"
+    end
+  end
+
   def to_s
     "ID: #{@id}, ФИО: #{@last_name} #{@first_name} #{@middle_name}, " +
     "Телефон: #{@phone || 'не указан'}, Telegram: #{@telegram || 'не указан'}, " +
