1bd52dc0694937f7431ba0872c11974b27c489f1
temawm <temafroloff@gmail.com>
2024-10-26 12:28:58 +0300

    lab2 Add_relation_diagramm

--- OOP_in_Ruby/student_short.rb
+++ OOP_in_Ruby/student_short.rb
@@ -1,13 +1,21 @@
 class Student_short < Person
   attr_reader :contact_info
 
-  def initialize(student:)
-    super(id: student.id, last_name: student.last_name, first_name: student.first_name, middle_name: student.middle_name, phone: student.phone, telegram: student.telegram, email: student.email, github: student.github)
-    @contact_info = generate_contact_info
-  end
+  def initialize(id: nil, contact_info: nil, student: nil)
+    if [id, contact_info].all? && student
+      raise ArgumentError, "Необходимо передать либо объект student, либо id и contact_info, но не все сразу"
+    end
 
-  def initialize(id:, contact_info:)
-    parse_contact_info(id, contact_info)
+    if student
+      super(id: student.id, last_name: student.last_name, first_name: student.first_name,
+            middle_name: student.middle_name, phone: student.phone, telegram: student.telegram,
+            email: student.email, github: student.github)
+      @contact_info = generate_contact_info
+    elsif id && contact_info
+      parse_contact_info(id, contact_info)
+    else
+      raise ArgumentError, "Необходимо передать либо объект student, либо id и contact_info"
+    end
   end
 
   private
@@ -22,7 +30,6 @@ class Student_short < Person
 
   def parse_contact_info(id, contact_info)
     @id = id
-
     match = contact_info.match(/(.+), GitHub: (.+), Телефон: (.+), Telegram: (.+)/)
     if match
       @last_name = match[1]
