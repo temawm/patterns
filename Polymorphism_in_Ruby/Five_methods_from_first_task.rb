18c6de4b26c9ed22dc6a6f1ea43cd721499049cb
temawm <temafroloff@gmail.com>
2024-11-30 00:11:47 +0300

    lab2_task1:_replace_first_part_after_min

--- Polymorphism_in_Ruby/Five_methods_from_first_task.rb
+++ Polymorphism_in_Ruby/Five_methods_from_first_task.rb
@@ -1,5 +1,12 @@
+
 def elements_after_last_max(array)
   last_max_index = array.rindex(array.max)
   count = array[last_max_index + 1..-1].size
   puts "Количество элементов после последнего максимального: #{count}"
+end
+
+def move_before_min_to_end(array)
+  min_index = array.index(array.min)
+  result = array[min_index..-1] + array[0...min_index]
+  puts "Новый массив: #{result.inspect}"
 end
