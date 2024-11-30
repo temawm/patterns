c9bb84c07cc9a937b757265bc0f234be395495b6
temawm <temafroloff@gmail.com>
2024-11-30 11:47:44 +0300

    lab3_task2:_reduce_method

--- Polymorphism_in_Ruby/Array_methods.rb
+++ Polymorphism_in_Ruby/Array_methods.rb
@@ -1,25 +1,31 @@
-def custom_find_all
-    result = []
-    @array.each do |el|
-      result << el if yield(el)
-    end
-    result
+class ArrayProcessor
+  def initialize(array)
+    @array = array.dup.freeze
   end
-  
-def custom_min_max
-    min = @array.first
-    max = @array.first
-    @array.each do |el|
-      min = el if el < min
-      max = el if el > max
-    end
-    [min, max]
-  end
-  
-def custom_reduce(initial)
-    accumulator = initial
-    @array.each do |el|
-      accumulator = yield(accumulator, el)
-    end
-    accumulator
-  end
+
+	def custom_find_all
+		result = []
+		@array.each do |el|
+		  result << el if yield(el)
+		end
+		result
+	  end
+	  
+	def custom_min_max
+		min = @array.first
+		max = @array.first
+		@array.each do |el|
+		  min = el if el < min
+		  max = el if el > max
+		end
+		[min, max]
+	  end
+	  
+	def custom_reduce(initial)
+		accumulator = initial
+		@array.each do |el|
+		  accumulator = yield(accumulator, el)
+		end
+		accumulator
+	end
+end
