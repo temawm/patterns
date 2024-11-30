41baeb53266a488bae8c41422d6e55668d79a362
temawm <temafroloff@gmail.com>
2024-11-30 11:43:21 +0300

    lab3_task2:_reduce_method

--- Polymorphism_in_Ruby/Array_methods.rb
+++ Polymorphism_in_Ruby/Array_methods.rb
@@ -15,3 +15,11 @@ def custom_min_max
     end
     [min, max]
   end
+  
+def custom_reduce(initial)
+    accumulator = initial
+    @array.each do |el|
+      accumulator = yield(accumulator, el)
+    end
+    accumulator
+  end
