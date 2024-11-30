63fce5e8f2b262553cc9e02c2fcef91f6cda19bb
temawm <temafroloff@gmail.com>
2024-11-30 13:37:15 +0300

    lab3_task1:_reworked_methods:_return_only_result_without_string

--- Polymorphism_in_Ruby/Array_methods.rb
+++ Polymorphism_in_Ruby/Array_methods.rb
@@ -22,7 +22,11 @@ class ArrayProcessor
 	  end
 	  
 	def custom_reduce(initial)
-		accumulator = initial
+		if initial != nil
+		  accumulator = initial
+		else
+		  accumulator = 0
+		end
 		@array.each do |el|
 		  accumulator = yield(accumulator, el)
 		end
@@ -51,3 +55,29 @@ class ArrayProcessor
   end
 	
 end
+
+
+
+
+
+processor = ArrayProcessor.new([1, 2, 3, 4, 5, 6])
+
+# find_all
+puts processor.custom_find_all { |el| el.even? }
+
+# min_max
+puts processor.custom_min_max.inspect
+
+# reduce
+puts processor.custom_reduce(0) { |sum, el| sum + el }
+
+# none?
+puts processor.custom_none? { |el| el < 0 }
+puts processor.custom_none? { |el| el > 5 }
+
+# find_index
+puts processor.custom_find_index { |el| el > 4 }
+
+# any?
+puts processor.custom_any? { |el| el > 4 }
+puts processor.custom_any? { |el| el < 0 }     
--- Polymorphism_in_Ruby/Five_methods_from_first_task.rb
+++ Polymorphism_in_Ruby/Five_methods_from_first_task.rb
@@ -8,26 +8,31 @@ def get_input_array
 end
 
 # Задача 1: Найти количество элементов, расположенных после последнего максимального
-def count_after_last_max(array)
-  max_index = array.each_with_index.max_by { |x, idx| x }.last
-  array.drop(max_index + 1).size
+def elements_after_last_max(array)
+  last_max_index = array.rindex(array.max)
+  count = array[last_max_index + 1..-1].size
 end
+
 
 # Задача 2: Разместить элементы до минимального в конце массива
 def move_before_min_to_end(array)
-  min_index = array.each_with_index.min_by { |x, idx| x }.last
-  array.drop(min_index).concat(array.take(min_index))
+  min_index = array.index(array.min)
+  result = array[min_index..-1] + array[0...min_index]
+  result.inspect
 end
 
 # Задача 3: Найти максимальный элемент в интервале a..b
 def max_in_interval(array, a, b)
-  array[a..b].max
+  puts "Максимальный элементв интервале: #{array[a..b].max}"
 end
 
 # Задача 4: Индексы элементов, которые меньше своего левого соседа, и их количество
 def indices_and_count_less_than_left(array)
-  indices = array.each_with_index.select { |x, idx| idx > 0 && x < array[idx - 1] }.map(&:last)
-  { indices: indices, count: indices.size }
+  def indices_and_count_of_smaller_than_left(array)
+	  indices = array.each_with_index.select { |el, idx| idx > 0 && el < array[idx - 1] }.map(&:last)
+	  count = indices.size
+	  { indices: indices.inspect, count: count }
+	end
 end
 
 # Задача 5: Простые делители всех элементов массива без повторений
@@ -39,7 +44,6 @@ def unique_prime_divisors(array)
   end.uniq
 end
 
-# Меню выбора задачи
 def task_menu
   puts <<-MENU
     Выберите задачу:
@@ -52,7 +56,6 @@ def task_menu
   gets.to_i
 end
 
-# Основная программа
 def main
   puts "Выберите источник данных: 1 - файл, 2 - клавиатура"
   source = gets.to_i
@@ -72,16 +75,17 @@ def main
 
   case task
   when 1
-    puts "Результат: #{count_after_last_max(array)}"
+	puts "Количество элементов после последнего максимального: #{elements_after_last_max(array)}"
   when 2
-    puts "Результат: #{move_before_min_to_end(array)}"
+    puts "Новый массив: #{move_before_min_to_end(array)}"
   when 3
     puts "Введите интервал (a b): "
     a, b = gets.split.map(&:to_i)
-    puts "Результат: #{max_in_interval(array, a, b)}"
+    puts "#{max_in_interval(array, a, b)}"
   when 4
     result = indices_and_count_less_than_left(array)
-    puts "Индексы: #{result[:indices]}, Количество: #{result[:count]}"
+    puts "Индексы элементов: #{result[:indices]}"
+	puts "Количество таких элементов: #{result[:count]}"
   when 5
     puts "Результат: #{unique_prime_divisors(array)}"
   else
