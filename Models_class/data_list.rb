420d4f01f6868f081e3806b12e185422c4acbaf2
temawm <temafroloff@gmail.com>
2025-01-22 15:54:34 +0300

    lab4_task3: add select, get_selected methods and get_names, get_data abstract methods

--- Models_class/data_list.rb
+++ Models_class/data_list.rb
@@ -1,9 +1,25 @@
+require_relative 'data_table'
+
 class DataList
   def initialize(elements)
     @elements = elements.sort
+    @selected = []
   end
 
-  def get_elements
-    @elements.dup
+  def select(number)
+    raise ArgumentError, "Некорректный номер" unless number.is_a?(Integer) && number.between?(0, @elements.size - 1)
+
+    @selected << number unless @selected.include?(number)
+  end
+  def get_selected
+    @selected.map { |index| @elements[index] }
+  end
+
+  def get_names
+    raise NotImplementedError, "Метод должен быть реализован в наследниках"
+  end
+
+  def get_data
+    raise NotImplementedError, "Метод должен быть реализован в наследниках"
   end
 end
