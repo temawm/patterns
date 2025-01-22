5b2455b66f07add87c831dcc038a54c6ce5ca4a3
temawm <temafroloff@gmail.com>
2025-01-22 15:46:58 +0300

    lav4_task2: add get_element method

--- Models_class/data_table.rb
+++ Models_class/data_table.rb
@@ -3,4 +3,16 @@ class DataTable
     raise ArgumentError, "Данные должны быть двумерным массивом" unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
     @data = data
   end
+
+  def get_element(row, col)
+    @data[row][col]
+  end
+
+  def columns_count
+    @data.empty? ? 0 : @data.first.size
+  end
+
+  def rows_count
+    @data.size
+  end
 end
